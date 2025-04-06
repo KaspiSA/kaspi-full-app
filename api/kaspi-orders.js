3470a8a2a0615a3516ce36e3bdd9457b2c3081cc
KaspiSA <zhmukhamet@gmail.com>
2025-04-07 04:33:53 +0500

    Update kaspi-orders.js

--- api/kaspi-orders.js
+++ api/kaspi-orders.js
@@ -3,7 +3,7 @@ const KASPI_API_KEY = process.env.KASPI_API_KEY;
 
 export default async function handler(req, res) {
   const response = await fetch(
-    "https://kaspi.kz/shop/api/v2/orders?page[number]=1&page[size]=10&filter[orders][creationDate][$ge]=2024-04-01",
+    "https://kaspi.kz/shop/api/v2/orders?page[number]=1&page[size]=10&filter[orders][creationDate][$ge]=2024-04-01T00:00:00.000Z",
     {
       headers: {
         "Content-Type": "application/json",
@@ -20,4 +20,5 @@ export default async function handler(req, res) {
 
   return res.status(200).json(data);
 }
+
 
